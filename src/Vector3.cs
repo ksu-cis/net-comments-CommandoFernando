56a7592a9a95956f016d3f1e94791d426044e427
Fernando Velarde <f.velarde@hotmail.com>
2019-09-16 14:21:37 -0500

    Added magnitude and cross product Classes.

--- src/Vector3.cs
+++ src/Vector3.cs
@@ -37,9 +37,9 @@ namespace VectorMath
         /// <summary>
         /// Constructs a new instance of Vector3 with
         /// the supplied values for X, Y, and Z.
-        /// <param name="X">A double precision number for the x component of the vector.</param>
-        /// <param name="Y">A double precision number for the y component of the vector.</param>
-        /// <param name="Z">A double precision number for the z component of the vector.</param>
+        /// <param name="x">A double precision number for the x component of the vector.</param>
+        /// <param name="y">A double precision number for the y component of the vector.</param>
+        /// <param name="z">A double precision number for the z component of the vector.</param>
         /// </summary>
         public Vector3(double x, double y, double z)
         {
@@ -47,5 +47,29 @@ namespace VectorMath
             this.Y = y;
             this.Z = z;
         }
+        /// <summary>
+        /// Calculates the cross product of this and
+        /// the other vector (definitely)
+        /// </summary>
+        /// <param name="other">The vector to cross with this one</param>
+        /// <returns>The cross product</returns>
+        public Vector3 CrossProduct(Vector3 other)
+        {
+            return new Vector3(
+                this.Y * other.Z - this.Z * other.Y, 
+                this.Z * other.X - this.X * other.Z,
+                this.X * other.Y - this.Y * other.X);
+        }
+        /// <summary>
+        /// Gets the magnitude of this Vector
+        /// </summary>
+        public double Magnitude
+        {
+            get
+            {
+                return Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
+            }
+        }
+
     }
 }
